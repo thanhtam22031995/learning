76996ced410e251a15a38733bb7587e2b5097b9c
DESKTOP-K9JV526\Tam <thanhtam22031995@gmail.com>
2020-07-11 02:39:10 +0700

    jul_11

--- jul_11/bt3.rb
+++ jul_11/bt3.rb
@@ -1,16 +1,8 @@
-require 'json'
-require 'stringio'
-
-# Complete the miniMaxSum function below.
-def miniMaxSum(arr)
+arr = gets.rstrip.split.map(&:to_i)
+min = max = arr.first
 a =  arr.reduce{|sum, x| sum + x}
-b = arr.sort.reverse
-c = arr.sort
-min = a - b[0]
-max = a - c[0]
-puts "#{min} #{max}"
+arr.each do |current|
+ min = current if current < min
+ max = current if current > max
 end
-
-arr = gets.rstrip.split(' ').map(&:to_i)
-
-miniMaxSum arr
+puts "#{a - max} #{a - min}"
